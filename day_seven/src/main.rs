bdefa943c310a6b8891be73984db7389b80475a1
Larqqa <32098438+Larqqa@users.noreply.github.com>
2021-12-07 17:03:18 +0200

    improved day seven

--- day_seven/src/main.rs
+++ day_seven/src/main.rs
@@ -1,62 +1,23 @@
-fn get_input() -> Vec<i32> {
-    return include_str!("input.txt")
-        .split(",")
-        .map(|s| s.trim().parse().unwrap())
-        .collect();
-}
-
-use std::collections::HashMap;
-fn part_one() -> i32 {
-    let input = get_input();
-    let sum = input.iter().max().unwrap();
-
-    let mut r = HashMap::new();
-    for n in 0..*sum {
-        *r.entry(n).or_insert(0) += input
-            .iter()
-            .fold(0, |a, b| a + if *b < n { n - b } else { b - n });
-    }
-
-    let mut min = (0, *r.get(&0).unwrap());
-    for (k, v) in r {
-        if v < min.1 {
-            min = (k, v);
-        }
-    }
-
-    min.1
-}
-
-fn part_two() -> i32 {
-    let input = get_input();
-    let sum = input.iter().max().unwrap();
-
-    let mut r = HashMap::new();
-    for n in 0..*sum {
-        *r.entry(n).or_insert(0) += input.iter().fold(0, |a, b| {
-            let c = if *b < n { n - b } else { b - n };
-            let mut d = 0;
-            let mut p = 1;
-            for n in 0..c {
-                d += p;
-                p += 1;
-            }
-
-            a + d
-        });
-    }
-
-    let mut min = (0, *r.get(&0).unwrap());
-    for (k, v) in r {
-        if v < min.1 {
-            min = (k, v);
-        }
-    }
-
-    min.1
-}
-
 fn main() {
-    println!("part one: {}", part_one());
-    println!("part two: {}", part_two());
+    let input: Vec<i32> = include_str!("input.txt")
+        .split(",")
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    println!(
+        "part one: {}\npart two: {}",
+        (0..*input.iter().max().unwrap())
+            .map(|n| input.iter().fold(0, |a, b| a + (b - n as i32).abs()))
+            .min()
+            .unwrap(),
+        (0..*input.iter().max().unwrap())
+            .map(|n| {
+                input.iter().fold(0, |a, b| {
+                    let c = (b - n as i32).abs();
+                    a + (c * (c + 1) / 2)
+                })
+            })
+            .min()
+            .unwrap()
+    );
 }
