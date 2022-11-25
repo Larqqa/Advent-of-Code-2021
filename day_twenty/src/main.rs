cb525b6dc02b8254904306e17c5ca4424d786491
Larqqa <32098438+Larqqa@users.noreply.github.com>
2022-11-25 10:47:23 +0200

    Working day 20 solution

--- day_twenty/src/main.rs
+++ day_twenty/src/main.rs
@@ -8,9 +8,11 @@ struct Image {
     width: usize,
     height: usize,
     data: Vec<char>,
+    void: bool,
 }
 
 impl Image {
+    #[allow(dead_code)]
     fn print_image(&self) {
         print!("\n");
         for y in 0..self.height {
@@ -20,6 +22,14 @@ impl Image {
             print!("\n");
         }
         print!("\n");
+    }
+
+    fn count_lights(&self) -> usize {
+        self.data
+            .clone()
+            .into_iter()
+            .filter(|x| *x == LIGHT)
+            .count()
     }
 }
 
@@ -36,6 +46,7 @@ fn get_input() -> (Vec<char>, Image) {
         width: data[0].len(),
         height: data.len(),
         data: data.concat(),
+        void: false,
     };
 
     (algo, image)
@@ -43,11 +54,12 @@ fn get_input() -> (Vec<char>, Image) {
 
 fn get_decimal_from_binary(str: &String) -> isize {
     isize::from_str_radix(&str, 2).unwrap()
-
 }
 
 fn get_binary(str: &String) -> String {
-    str.chars().map(|x| if x == LIGHT {'1'} else {'0'}).collect()
+    str.chars()
+        .map(|x| if x == LIGHT { '1' } else { '0' })
+        .collect()
 }
 
 fn run_iter(algo: &Vec<char>, image: &Image) -> Image {
@@ -55,73 +67,79 @@ fn run_iter(algo: &Vec<char>, image: &Image) -> Image {
         width: image.width + 2,
         height: image.height + 2,
         data: vec![],
+        void: image.void,
     };
 
+    // Map enhanced image
     for y in 0..output.height {
         for x in 0..output.width {
             let first_x = x as i32 - 1;
             let first_y = y as i32 - 1;
             let mut identifier = String::new();
 
-            for y2 in first_y..(y + 2) as i32 {
-                let mut line = String::new();
-                for x2 in first_x..(x + 2) as i32 {
-                    let x3 = x2 - 1;
-                    let y3 = y2 - 1;
+            // Map 3x3 region around pixel
+            for region_y in first_y..(y + 2) as i32 {
+                let mut pixel_row = String::new();
 
-                    let c = if x3 < 0 || y3 < 0 || x3 as usize >= image.width || y3 as usize >= image.height {
-                        DARK
-                    } else {
-                        let i = image.width * y3 as usize + x3 as usize;
-                        if i < image.data.len() {
-                            image.data[i]
-                        } else {
-                            DARK
+                for region_x in first_x..(x + 2) as i32 {
+                    let image_x = region_x - 1;
+                    let image_y = region_y - 1;
+
+                    // If mapped pixel is outside of original image, add void pixel
+                    let pixel = if image_x < 0
+                        || image_y < 0
+                        || image_x as usize >= image.width
+                        || image_y as usize >= image.height
+                    {
+                        match output.void {
+                            true => LIGHT,
+                            _ => DARK,
                         }
+                    } else {
+                        image.data[image.width * image_y as usize + image_x as usize]
                     };
-                    line.push(c);
+                    pixel_row.push(pixel);
                 }
-
-                identifier.push_str(line.as_str());
-            }
-            if identifier.contains(LIGHT)  {
-                let bin = get_binary(&identifier);
-                let num = get_decimal_from_binary(&bin);
-                // println!("{} {}", bin, num);
-                output.data.push(algo[num as usize]);
-            } else {
-                output.data.push(DARK);
+                identifier.push_str(pixel_row.as_str());
             }
 
+            let bin = get_binary(&identifier);
+            let num = get_decimal_from_binary(&bin);
+            output.data.push(algo[num as usize]);
         }
     }
 
-    // output.print_image();
+    // Flicker void if algorithm dictates the flicker
+    output.void = if !output.void && algo[0] == LIGHT {
+        true
+    } else if output.void && algo.last() == Some(&DARK) {
+        false
+    } else {
+        output.void
+    };
 
     output
 }
 
 fn part_one() -> usize {
     let (algo, image) = get_input();
-    // println!("{:?}", algo);
-    // println!("{:?}", image);
-
-    image.print_image();
     let mut o = image;
-    // for _ in 0..2 {   
-    //     o = run_iter(&algo, &o);
-    // }
-    o = run_iter(&algo, &o);
-    o.print_image();
-
-    o.data.into_iter().filter(|x| *x == LIGHT).count()
+    for _ in 0..2 {
+        o = run_iter(&algo, &o);
+    }
+    o.count_lights()
 }
 
-// fn part_two() -> i32 {
-//     0
-// }
+fn part_two() -> usize {
+    let (algo, image) = get_input();
+    let mut o = image;
+    for _ in 0..50 {
+        o = run_iter(&algo, &o);
+    }
+    o.count_lights()
+}
 
 fn main() {
     println!("part one: {}", part_one());
-    // println!("part two: {}", part_two());
+    println!("part two: {}", part_two());
 }
